27544e68b9fadb1cb5024ea5f89cd26995905edd
Rohit_More <morer4851@gmail.com>
2025-12-12 22:17:24 +0530

    Implemented get_data to get the required weather data

--- backend.py
+++ backend.py
@@ -18,7 +18,6 @@ def get_data(place, forecast_days=None, kind=None):
 	else:
 		filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
 		
-	
 	return filtered_data
 
 if __name__ == "__main__":
