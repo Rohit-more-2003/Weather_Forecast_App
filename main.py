5c9f308311f6ba328af1851e44be5623be111dd9
Rohit_More <morer4851@gmail.com>
2025-12-12 21:21:00 +0530

    Implemented get_data to get the required weather data

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import streamlit as st
+#Data visualisation library to plot graph
+import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
-
 place = st.text_input("Place: ").title()
-
 days = st.slider("Forecast Days",
                  min_value=1, max_value=5,
                  help="Select the number of forecasted days")
-
 option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
-
 st.subheader(f"{option} for the next {days} days in {place}")
+
+data = get_data(days, place, option)
+
+#d, t = get_data(days)
+
+figure = px.line(x=d, y=t,
+                 labels={"x":"Date", "y":"Temperature (C)"})
+st.plotly_chart(figure)
